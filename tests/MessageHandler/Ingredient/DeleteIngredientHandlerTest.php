0083b5dd206e96dbf4a422df37897e20a29b564b
sylvain-pc-amiltone <sylvainriviereweb@gmail.com>
2025-05-01 22:23:25 +0200

    dev: delete ingredient test

---
+++ tests/MessageHandler/Ingredient/DeleteIngredientHandlerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\MessageHandler\Ingredient;
+
+use App\DataFixtures\IngredientFixture;
+use App\Entity\Ingredient;
+use App\MessageHandler\Ingredient\DeleteIngredient\DeleteIngredientCommand;
+use App\MessageHandler\Ingredient\DeleteIngredient\DeleteIngredientHandler;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * @covers \App\MessageHandler\Ingredient\DeleteIngredient\DeleteIngredientHandler
+ */
+final class DeleteIngredientHandlerTest extends KernelTestCase
+{
+    private EntityManagerInterface $em;
+    private DeleteIngredientHandler $handler;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $this->em = self::getContainer()->get(EntityManagerInterface::class);
+        $this->handler = self::getContainer()->get(DeleteIngredientHandler::class);
+
+        // Isolation transactionnelle
+        $this->em->beginTransaction();
+
+        // On charge la catégorie + l'ingrédient basique
+        $loader = new Loader();
+        $loader->addFixture(new IngredientFixture());
+
+        (new ORMExecutor($this->em, new ORMPurger()))
+            ->execute($loader->getFixtures(), append: true);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->em->rollback();
+        $this->em->close();
+        parent::tearDown();
+    }
+
+    public function testItDeletesAnIngredient(): void
+    {
+        /** @var Ingredient|null $ingredient */
+        $ingredient = $this->em
+            ->getRepository(Ingredient::class)
+            ->findOneBy(['name' => IngredientFixture::ORIGINAL_NAME]);
+        $ingredientId = $ingredient->getId();
+
+        self::assertNotNull($ingredient, "L'ingrédient fixture n'existe pas");
+
+        $command = new DeleteIngredientCommand($ingredientId);
+
+        ($this->handler)($command);
+
+        $deleted = $this->em->getRepository(Ingredient::class)->find($ingredientId);
+        self::assertNull($deleted, 'L’ingrédient devrait être supprimé de la base');
+    }
+}
